8a9c8b055257c54794920343c1a1cd6979244243
Guillaume-Levrier <Guillaume-Levrier@users.noreply.github.com>
2019-03-22 17:10:09 +0100

    style update

--- js/scroller.js
+++ js/scroller.js
@@ -86,86 +86,89 @@ const display = () => {
 
 var scroll = scroller().container(d3.select('#graphic'));
 
+var iframe = document.querySelector('iframe');
+var iframeContent = iframe.contentWindow.document.body;
+var source = document.getElementById('source');
 scroll(d3.selectAll('.step'));
 
         scroll.on('active', function (index) {
                 d3.selectAll('.step').style('opacity', function (d, i) { return i === index ? 1 : 0.1; });
                              
-// HE jiankui video https://www.youtube.com/watch?v=th0vnOmFltc
                               let sourceCredit = "Source/Credit: ";
-                              switch (index) {
-                                case 0: //starting point
-                                      document.getElementById('iframe').src = "videoloop1.html";
-                                      document.getElementById('source').innerText = sourceCredit + "Wired - https://www.wired.com/story/wired-guide-to-crispr/";
+                              var sectionList = document.querySelectorAll("section");
+                              
+                              switch (sectionList[index].id) {
+                                case "introslide": 
+                                          iframeContent.innerHTML = '<video id="video" loop autoplay muted onclick="this.paused ? this.play() : this.pause();" width="600px;"><source src="video/loop.mp4"  type="video/mp4"></video>';
+                                          source.innerText = sourceCredit + "Wired - https://www.wired.com/story/wired-guide-to-crispr/";
                                       break;
-                                case 1: //1 
-                                      document.getElementById('iframe').src = "img/agamben-foucault.png";
-                                      document.getElementById('iframe').contentDocument.body.width = '600px';
+                                case "foucagamb":  
+                                iframeContent.innerHTML = '<img width="600px" src="img/agamben-foucault.png"/>';
 
-                                      document.getElementById('source').innerText = "";
+                                      source.innerText = "";
                                       break;
             
-                                case 2: //Theroux
-                                      document.getElementById('iframe').src = "";
-                                      document.getElementById('source').innerText = sourceCredit + "@louistherouxbot by Andy Kelly - Twitter";
+                                case "accident": 
+                                     // iframe.src = "";
+                                      //source.innerText = sourceCredit + "@louistherouxbot by Andy Kelly - Twitter";
                                       break;
             
-                                case 3: //Mapping
-                                      document.getElementById('iframe').src = "https://imgs.xkcd.com/comics/map_projections.png";
-                                      document.getElementById('source').innerText = sourceCredit + "xkcd.com/977 by Randall Munroe - XKCD";
+                                case "crisprXplainer": 
+                                iframeContent.innerHTML = '<video id="video" loop="" autoplay="" muted="" onclick="this.paused ? this.play() : this.pause();" width="600px;"><source src="video/crisprExplainerloop.mp4"  type="video/mp4"></video>';
+                                      source.innerText = sourceCredit + "";
                                       break;
             
                                 case 4: //PornVsReality
-                                      document.getElementById('iframe').src = "img/soska117meme.png";
-                                      document.getElementById('source').innerText = sourceCredit + "@soska_117 - Instagram / edited by Guillaume Levrier";
+                                      iframe.src = "img/soska117meme.png";
+                                      source.innerText = sourceCredit + "@soska_117 - Instagram / edited by Guillaume Levrier";
                                       break;
             
                                 case 5: //NationBuilder
-                                      document.getElementById('iframe').src = "https://nationbuilder.com/";
-                                      document.getElementById('source').innerText = sourceCredit + "nationbuilder.com";
+                                      iframe.src = "https://nationbuilder.com/";
+                                      source.innerText = sourceCredit + "nationbuilder.com";
                                       break;
             
                                 case 6: //MeToo
-                                      document.getElementById('iframe').src = "img/timecoversilencebreakers.png";
-                                      document.getElementById('source').innerText = sourceCredit + "Time’s Person of the Year issue 2017";
+                                      iframe.src = "img/timecoversilencebreakers.png";
+                                      source.innerText = sourceCredit + "Time’s Person of the Year issue 2017";
                                       break;
                                 case 7: //Cash
-                                      document.getElementById('iframe').src = "https://www.nytimes.com/interactive/2018/01/27/technology/social-media-bots.html";
-                                      document.getElementById('source').innerText = sourceCredit + "NICHOLAS CONFESSORE, GABRIEL J.X. DANCE, RICHARD HARRIS and MARK HANSEN - 27/01/2018 - The New York Times";
+                                      iframe.src = "https://www.nytimes.com/interactive/2018/01/27/technology/social-media-bots.html";
+                                      source.innerText = sourceCredit + "NICHOLAS CONFESSORE, GABRIEL J.X. DANCE, RICHARD HARRIS and MARK HANSEN - 27/01/2018 - The New York Times";
                                       break;
                                 case 8: //meta
-                                      document.getElementById('iframe').src = "blank.html";
-                                      document.getElementById('source').innerText = "";
+                                      iframe.src = "blank.html";
+                                      source.innerText = "";
                                       break;
             
                                 case 9: //findingData
-                                      document.getElementById('iframe').src = "img/china-surveillance-ai.jpg";
-                                      document.getElementById('source').innerText = sourceCredit + "REUTERS/Bobby Yip";
+                                      iframe.src = "img/china-surveillance-ai.jpg";
+                                      source.innerText = sourceCredit + "REUTERS/Bobby Yip";
                                       break;
             
                                 case 10: //ShedANewLight
-                                      document.getElementById('iframe').src = "  https://lemonde.fr/mmpub/edt/zip/2018/06/26/134110911-93bc7e170115d2edd843aef16eaf6a7eaedb6248/index.html";
-                                      document.getElementById('source').innerText = sourceCredit + "Le Monde/Les Décodeurs, Agathe Dahyot, Pierre Breteau et Adrien Sénécat";
+                                      iframe.src = "  https://lemonde.fr/mmpub/edt/zip/2018/06/26/134110911-93bc7e170115d2edd843aef16eaf6a7eaedb6248/index.html";
+                                      source.innerText = sourceCredit + "Le Monde/Les Décodeurs, Agathe Dahyot, Pierre Breteau et Adrien Sénécat";
                                       break;
             
                                 case 11: //Squad system
-                                      document.getElementById('iframe').src = "ncase-crowds/index.html";
-                                      document.getElementById('source').innerText = sourceCredit + "Nicky Case (@ncase) -  https://ncase.me/crowds/";
+                                      iframe.src = "ncase-crowds/index.html";
+                                      source.innerText = sourceCredit + "Nicky Case (@ncase) -  https://ncase.me/crowds/";
                                       break;
             
                                 case 12: //Grades
-                                      document.getElementById('iframe').src = "grades.html";
-                                      document.getElementById('source').innerText =  "";
+                                      iframe.src = "grades.html";
+                                      source.innerText =  "";
                                       break;
             
                                 case 13: //Rules
-                                      document.getElementById('iframe').src = "rules.html";
-                                      document.getElementById('source').innerText = sourceCredit + "Sciences Po";
+                                      iframe.src = "rules.html";
+                                      source.innerText = sourceCredit + "Sciences Po";
                                       break;
             
                                 case 14: //Final remarks
-                                      document.getElementById('iframe').src = "https://www.youtube.com/embed/IOkHL8CC1y4?autoplay=1&mute=1#modestbranding=1&controls=0";
-                                      document.getElementById('source').innerText = sourceCredit + "Baron Noir - Kwaï/Studiocanal";
+                                      iframe.src = "https://www.youtube.com/embed/IOkHL8CC1y4?autoplay=1&mute=1#modestbranding=1&controls=0";
+                                      source.innerText = sourceCredit + "Baron Noir - Kwaï/Studiocanal";
                                       break;
             
                               };
