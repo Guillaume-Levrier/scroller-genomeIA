ec1f1b4ecea57864b9a8194cf1b20bb71779d119
Guillaume-Levrier <Guillaume-Levrier@users.noreply.github.com>
2019-03-23 12:04:10 +0100

    adding content

--- js/scroller.js
+++ js/scroller.js
@@ -114,71 +114,32 @@ scroll(d3.selectAll('.step'));
                                       break;
 
                                 case "plan": 
-                                iframeContent.innerHTML = '';
+                                iframeContent.innerHTML = 
+                                "<span style='font-family:sans-serif'><h2>I - L’édition du génome est une technique prométhéenne qui incarne le processus par lequel la science crée de nouveaux pouvoirs.</h2>"+
+                                "<h3><i>A/ Effacer, ajouter, modifier l’ADN : entrer dans l’âge des nucléases<br>B/ De la technique à la science, de la science au pouvoir : biosphère et biopouvoir au XXIème siècle</i></h3>"+
+                                "<h2>II – Les nucléases potentialisent fortement le génie génétique, alors que la plupart des Etats peinent à prendre à bras le corps la question du vivant en politique</h2>"+
+                                "<h3><i>A/ Le développement des capacités scientifiques en génie génétique s’est brusquement accéléré avec CRISPR<br>B/ Tous les Etats ne sont pas égaux face à la nécessité de débattre du vivant</i></h3>"
+                                +"</span>";
                                 source.innerText = sourceCredit + "@photosource";
                                       break;
 
                                 case "DNAexplainer": 
-                                iframeContent.innerHTML = '<img width="600px" src="img/DNA_Nucleotides.jpg"/>';
+                                iframeContent.innerHTML = '<img height="450px" src="img/DNA_Nucleotides.jpg"/>';
                                 source.innerText = sourceCredit + "OpenStax Anatomy and Physiology";
                                       break;
                                 case "crisprXplainer": 
                                 iframeContent.innerHTML = '<video id="video" loop="" autoplay="" muted="" onclick="this.paused ? this.play() : this.pause();" width="600px;"><source src="video/crisprExplainerloop.mp4"  type="video/mp4"></video>';
-                                      source.innerText = sourceCredit + "";
-                                      break;
-            
-                                case 4: //PornVsReality
-                                      iframe.src = "img/soska117meme.png";
-                                      source.innerText = sourceCredit + "@soska_117 - Instagram / edited by Guillaume Levrier";
-                                      break;
-            
-                                case 5: //NationBuilder
-                                      iframe.src = "https://nationbuilder.com/";
-                                      source.innerText = sourceCredit + "nationbuilder.com";
-                                      break;
-            
-                                case 6: //MeToo
-                                      iframe.src = "img/timecoversilencebreakers.png";
-                                      source.innerText = sourceCredit + "Time’s Person of the Year issue 2017";
-                                      break;
-                                case 7: //Cash
-                                      iframe.src = "https://www.nytimes.com/interactive/2018/01/27/technology/social-media-bots.html";
-                                      source.innerText = sourceCredit + "NICHOLAS CONFESSORE, GABRIEL J.X. DANCE, RICHARD HARRIS and MARK HANSEN - 27/01/2018 - The New York Times";
-                                      break;
-                                case 8: //meta
-                                      iframe.src = "blank.html";
-                                      source.innerText = "";
-                                      break;
-            
-                                case 9: //findingData
-                                      iframe.src = "img/china-surveillance-ai.jpg";
-                                      source.innerText = sourceCredit + "REUTERS/Bobby Yip";
-                                      break;
-            
-                                case 10: //ShedANewLight
-                                      iframe.src = "  https://lemonde.fr/mmpub/edt/zip/2018/06/26/134110911-93bc7e170115d2edd843aef16eaf6a7eaedb6248/index.html";
-                                      source.innerText = sourceCredit + "Le Monde/Les Décodeurs, Agathe Dahyot, Pierre Breteau et Adrien Sénécat";
-                                      break;
-            
-                                case 11: //Squad system
-                                      iframe.src = "ncase-crowds/index.html";
-                                      source.innerText = sourceCredit + "Nicky Case (@ncase) -  https://ncase.me/crowds/";
-                                      break;
-            
-                                case 12: //Grades
-                                      iframe.src = "grades.html";
-                                      source.innerText =  "";
-                                      break;
-            
-                                case 13: //Rules
-                                      iframe.src = "rules.html";
-                                      source.innerText = sourceCredit + "Sciences Po";
-                                      break;
-            
-                                case 14: //Final remarks
-                                      iframe.src = "https://www.youtube.com/embed/IOkHL8CC1y4?autoplay=1&mute=1#modestbranding=1&controls=0";
-                                      source.innerText = sourceCredit + "Baron Noir - Kwaï/Studiocanal";
-                                      break;
+                                source.innerText = sourceCredit + "";
+                                break;
+
+                                case "anthropocene": 
+                                iframeContent.innerHTML = '<video id="video" onclick="this.paused ? this.play() : this.pause();" width="600px;"><source src="video/latourAnthropocene.mp4"  type="video/mp4"></video>';
+                                source.innerText = sourceCredit + "";
+                                break;
+                              case "humanCRISPR": 
+                              iframeContent.innerHTML = '<video id="video" loop="" autoplay="" muted="" onclick="this.paused ? this.play() : this.pause();" width="600px;"><source src="video/zaynertrailer.mp4"  type="video/mp4"></video>';
+                              source.innerText = sourceCredit + "";
+                              break;
             
                               };
                         });
